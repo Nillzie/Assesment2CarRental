9a94a1a55711faca94500cc4407f6f5311c6cf76
Nillzie <nillzie@live.com.au>
2014-05-01 09:29:57 +0800

    Added some of PricePerKm

--- Ass2Pha2/src/PerKmRental.java
+++ Ass2Pha2/src/PerKmRental.java
@@ -1,4 +1,18 @@
 
 public class PerKmRental {
+	
+	
+	public PerKmRental(int startOdo, int endOdo, int pricePerKm, int rPrice)
+	{
+		int outOdometer = startOdo;
+		int inOdometer = endOdo;
+		int pricePerK = pricePerKm;
+		int rentalPrice = rPrice;
+		
+	}
+
+
+	
+
 
 }
